c9e8a13b0feaef81b2d07c322bdf50153a704405
markreyes-aseglobal <mark.reyes@ase-global.com>
2024-04-17 15:36:33 +0800

    Removed [FromForm] for IFormFile

--- Controllers/FileImportController.cs
+++ Controllers/FileImportController.cs
@@ -42,7 +42,7 @@ namespace hackathon_file_import.Controllers
         [HttpPost("upload")]
         [SwaggerOperation(Tags = new[] { "File Import" })]
         [AuthorizeAttribute(Roles: new string[] { "ReadOnly", "User" })]
-        public IActionResult Upload([FromForm]IFormFile file,string userId)
+        public IActionResult Upload(IFormFile file,string userId)
         {
             if (!_fileImportService.IsValidFile(file))
             {
